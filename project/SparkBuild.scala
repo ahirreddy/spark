22413b350e14d5d5103be3d8dce07660f86283fd
Ahir Reddy <ahirreddy@gmail.com>
2014-04-08 15:29:55 -0700

    Added pyrolite dependency

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -329,7 +329,7 @@ object SparkBuild extends Build {
         "com.twitter"                % "chill-java"       % "0.3.1" excludeAll(excludeAsm),
         "org.tachyonproject"         % "tachyon"          % "0.4.1-thrift" excludeAll(excludeHadoop, excludeCurator, excludeEclipseJetty, excludePowermock),
         "com.clearspring.analytics"  % "stream"           % "2.5.1",
-        "net.razorvine" % "pyrolite_2.10" % "1.1"
+        "org.spark-project"          % "pyrolite"         % "2.0"
       ),
     libraryDependencies ++= maybeAvro
   )
